7f76ecba036dfdb6819dc59ef296cad9d6cc265e
JingpuRen <yy5396681@qq.com>
2025-02-21 23:28:35 +0800

    Add a custom dedicated serialization method for the mobile phone number field

--- admin/src/main/java/org/gopher/shortlink/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/org/gopher/shortlink/admin/dto/resp/UserRespDTO.java
@@ -1,6 +1,8 @@
 package org.gopher.shortlink.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
+import org.gopher.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 
 /**
  * 当你从控制器方法返回一个实体类对象时，通常会使用 Spring MVC 等框架将对象转换为 JSON 或其他格式返回给客户端。
@@ -12,6 +14,9 @@ public class UserRespDTO {
     private Long	id;
     private String	username;
     private String	realName;
+    // 在默认情况下序列化的时候，都是序列化成JSON，但由于电话是比较重要的隐私
+    // 因此我们在这里改用封装好的序列化器
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String	phone;
     private String	mail;
 }

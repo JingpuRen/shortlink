855b038e227ba26a1c0793c1c222787c5548b555
JingpuRen <yy5396681@qq.com>
2025-02-21 23:26:51 +0800

    update

--- admin/src/main/java/org/gopher/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/gopher/shortlink/admin/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.gopher.shortlink.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.gopher.shortlink.admin.common.convention.result.Result;
+import org.gopher.shortlink.admin.common.convention.result.Results;
 import org.gopher.shortlink.admin.dto.resp.UserRespDTO;
 import org.gopher.shortlink.admin.service.UserService;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +25,9 @@ public class UserController {
      * @return
      */
     @PostMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username") String username){
-        return userService.getUserByUsername(username);
+    // 返回的虽然是Result，但是SpringBoot框架会默认将返回的结果序列化成JSON格式
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
+        UserRespDTO userRespDTO = userService.getUserByUsername(username);
+        return Results.success(userRespDTO);
     }
 }

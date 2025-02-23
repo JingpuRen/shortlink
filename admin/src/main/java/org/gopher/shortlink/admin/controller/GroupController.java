1d0c3d5818f3c1831b13263ac817c68a88e6589c
JingpuRen <yy5396681@qq.com>
2025-02-23 23:28:48 +0800

    update

---
+++ admin/src/main/java/org/gopher/shortlink/admin/controller/GroupController.java
@@ -0,0 +1,12 @@
+package org.gopher.shortlink.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.gopher.shortlink.admin.service.GroupService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GroupController {
+    private final GroupService groupService;
+
+}
--- admin/src/main/java/org/gopher/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/org/gopher/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,12 +1,16 @@
 package org.gopher.shortlink.admin.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import groovy.util.logging.Slf4j;
 import org.gopher.shortlink.admin.dao.entity.GroupDO;
 import org.gopher.shortlink.admin.dao.mapper.GroupMapper;
 import org.gopher.shortlink.admin.service.GroupService;
+import org.springframework.stereotype.Service;
 
 /**
  * 短链接分组接口实现层
  */
+@Service
+@Slf4j
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
 }

c0e9fd875e99d09d4501ae32a3cf2168d647a92c
JingpuRen <yy5396681@qq.com>
2025-02-27 11:53:19 +0800

    短链接常量类：设置永久短链接默认的缓存有效时间

---
+++ project/src/main/java/org/gopher/shortlink/project/common/constant/ShortLinkConstant.java
@@ -0,0 +1,11 @@
+package org.gopher.shortlink.project.common.constant;
+
+/**
+ * 短链接常量类
+ */
+public class ShortLinkConstant {
+    /**
+     * 永久短链接默认的缓存有效时间
+     */
+    public static final long DEFAULT_CACHE_VALID_DATE = 2626560000L;
+}

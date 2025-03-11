24f0d94791636e00f5ef0a20a6e5f9f6490bb469
JingpuRen <yy5396681@qq.com>
2025-03-12 00:28:09 +0800

    短链接监控Mapper构造

--- project/src/main/java/org/gopher/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
+++ project/src/main/java/org/gopher/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
@@ -10,6 +10,6 @@ import org.gopher.shortlink.project.dao.entity.LinkAccessStatsDO;
 public interface LinkAccessStatsMapper extends BaseMapper<LinkAccessStatsDO> {
     @Insert("INSERT INTO t_link_access_stats (full_short_url,gid,date,pv,uv,uip,hour,weekday,create_time,update_time,del_flag) " +
             "VALUES (#{linkAccessStats.fullShortUrl},#{linkAccessStats.gid},#{linkAccessStats.date},#{linkAccessStats.pv},#{linkAccessStats.uv},#{linkAccessStats.uip},#{linkAccessStats.hour},#{linkAccessStats.weekday},NOW(),NOW(),0) ON DUPLICATE KEY UPDATE " +
-            "pv = pv + 1, uv = uv + 2, uip = uip + 3;")
+            "pv = pv + 1, uv = uv + #{linkAccessStats.uv}, uip = uip + #{linkAccessStats.uip};")
     void shortLinkStats(@Param("linkAccessStats") LinkAccessStatsDO linkAccessStatsDO);
 }

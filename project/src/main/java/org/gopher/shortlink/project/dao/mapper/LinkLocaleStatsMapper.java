75b1cd969e923ab54356be9ea9e7aa5dce652f06
JingpuRen <yy5396681@qq.com>
2025-03-12 22:56:20 +0800

    增加注意点

--- project/src/main/java/org/gopher/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
+++ project/src/main/java/org/gopher/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -11,6 +11,7 @@ import org.gopher.shortlink.project.dao.entity.LinkLocaleStatsDO;
  */
 @Mapper
 public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
+    // tip : 记得调这个时候是调shortLinkLocaleStats而不是调insert
     @Insert("INSERT INTO t_link_locale_stats(full_short_url,gid,date,cnt,province,city,adcode,country,create_time,update_time,del_flag) " +
             "VALUES (#{linkLocaleStats.fullShortUrl},#{linkLocaleStats.gid},#{linkLocaleStats.date},#{linkLocaleStats.cnt},#{linkLocaleStats.province},#{linkLocaleStats.city},#{linkLocaleStats.adcode},#{linkLocaleStats.country},NOW(),NOW(),0) ON DUPLICATE KEY UPDATE " +
             "cnt = cnt + #{linkLocaleStats.cnt};")
